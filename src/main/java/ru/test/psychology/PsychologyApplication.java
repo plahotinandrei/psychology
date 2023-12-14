03e13f1ef79489d03a925b6c4c233d4e43667c8c
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2023-12-14 15:26:24 +0300

    init project

---
+++ src/main/java/ru/test/psychology/PsychologyApplication.java
@@ -0,0 +1,13 @@
+package ru.test.psychology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsychologyApplication.class, args);
+    }
+
+}
